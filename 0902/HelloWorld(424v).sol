68bae51404954ba7584ae4d68bac3ee3efd61c4f
김도림 <dorim1209@gmail.com>
2019-09-03 17:26:48 +0900

    19.09.02에 배운 내용

---
+++ 0902/HelloWorld(424v).sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.11;
+
+contract HelloWorld {
+    string public greeting;
+    
+    function HelloWorld(string _greeting) {
+        greeting = _greeting;
+        
+    }
+    
+    function setGreeting(string _greeting) {
+        greeting = _greeting;
+    }
+    
+    function say() constant returns (string) {
+        return greeting;
+    }
+}
