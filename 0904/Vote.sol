e57b67e1456918894abc286c2510d392cd3df3e3
김도림 <dorim1209@gmail.com>
2019-09-04 17:25:17 +0900

    vote 수정

--- 0904/Vote.sol
+++ 0904/Vote.sol
@@ -1,34 +1,64 @@
 pragma solidity ^0.5.8;
+// duplicate => bool (vote)
+// add candidate => ownerOnly (addCandidate)
 
 contract Vote {
-    uint8 numOfCandidate =0;
-    mapping(string => uint) score; // 후보자 리스트
-    mapping(uint => string) candidateList; // 각 후보자별 득표수
-
+    uint8 numOfCandidate = 0;
+    mapping(address => bool) isVoted; 
+    mapping(string => uint) score; // 각 후보자별 득표수
+    mapping(uint8 => string) candidateList; // 후보자 리스트
+    address payable owner;
+    
     constructor () public {
-
+        owner = msg.sender;
     }
 
-    function addCandidate (string memory candidate) public {
+    modifier ownerOnly () {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    modifier checkDuplicate() {
+        require(!isVoted[msg.sender]);
+        _;
+    }
+
+    function addCandidate (string memory candidate) public ownerOnly {
+        bool found = false;
+        for (uint8 i=0; i<numOfCandidate; i++) {
+            if(keccak256(bytes(candidateList[i])) == keccak256(bytes(candidate))) {
+                found = true;
+                break;
+            }    
+        
+        }
+        
+        require (!found);
+        
         candidateList[numOfCandidate] = candidate;
         numOfCandidate ++;
     }
 
-    function vote (string memory candidate) public {
+    function vote (string memory candidate) public checkDuplicate {
+        
         score[candidate] ++;
+        isVoted[msg.sender] = true;
     }
 
     function getNumOfCandidate() public view returns (uint8) {
         return numOfCandidate;
-
     }
 
-    function getCandidate(uint8 index) public view returns (string memory) {
+    function getCandidate(uint8 index) public view  returns (string memory) {
         return candidateList[index];
-
     }
 
     function getScore(string memory candidate) public view returns (uint) {
         return score[candidate];
+    }
+    
+    // destroy contract
+    function killContract() public ownerOnly {
+        selfdestruct(owner);
     }
 }
