c29128c9b6b5f409d57fbd1fcb394333610b931c
김도림 <dorim1209@gmail.com>
2019-09-10 15:13:51 +0900

    수정

--- 0909/mytoken/truffle-config.js
+++ 0909/mytoken/truffle-config.js
@@ -1,11 +1,22 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
+ 
+var infura_apikey = process.env.API_KEY;
+var mnemonic = process.env.MNEMONIC;
+ 
+console.log("infura_apikey = ", infura_apikey);
+console.log("mnemonic = ", mnemonic);
+ 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+      network_id: 3
     }
   }
 };
